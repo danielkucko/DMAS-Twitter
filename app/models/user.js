4703bd58054fb19faba8c3e765375b9aa5a5cf64
daniel.kucko <daniel.kucko@freenet.de>
2016-11-09 10:45:21 +0100

    fixed the user model

--- app/models/user.js
+++ app/models/user.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
   firstName: String,
-  lastNae: String,
+  lastName: String,
   email: String,
   password: String,
 });
